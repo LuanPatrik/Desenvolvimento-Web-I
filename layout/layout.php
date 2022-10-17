50ff2def89a5c3c3b7e4e9e132c83e295639360b
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2022-10-17 16:58:50 -0300

    Update

---
+++ layout/layout.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/layout.css">
+    <title>One Piece</title>
+</head>
+<body>
+    <div class="banner">
+        <img src="../imagens/layout/opBanner.jpg" alt="Banner">
+    </div>
+    <div class="titulo">
+        <h1>Bando do Chapéu de Palha</h1>
+        <p>
+            Os Piratas do Chapéu de Palha são uma tripulação pirata 
+            ascendente e infame que se originou do East Blue, mas que
+            possui vários membros de locais diferentes. 
+        </p>
+    </div>
+    <div class="informativo">
+        <h2>Tripulação</h2>
+        <p>Monkey D. luffy</p>
+        <p>Roronoa Zoro</p>
+        <p>Nami</p>
+        <p>Usopp</p>
+        <p>Sanji Vinsmoker</p>
+        <p>Tony Tony Chopper</p>
+        <p>Nico Robin</p>
+        <p>Franky</p>
+        <p>Brook</p>
+        <p>Jinbe</p>
+    </div>
+</body>
+</html>
