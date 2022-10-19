4aee89f4d80241f6bd063623bc630ece59b6bfd2
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2022-10-19 10:56:31 -0300

    Update Layout

---
+++ layout/layout.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/layout.css">
+    <title>One Piece</title>
+</head>
+<body>
+    <div class="banner">
+        <img src="../imagens/layout/opBanner.jpg" alt="Banner">
+    </div>
+    <div class="titulo">
+        <h1>Bando do Chapéu de Palha</h1>
+        <p>
+            Os Piratas do Chapéu de Palha são uma tripulação pirata 
+            ascendente e infame que se originou do East Blue, mas que
+            possui vários membros de locais diferentes. 
+        </p>
+    </div>
+    <div class="informativo">
+        <div class="tripulacaoLinha">
+            <div class="tripulacao">
+                <p>Monkey D. luffy</p>
+                <img src="../imagens/layout/luffy.webp" alt="Luffy">
+            </div>
+            <div class="tripulacao">
+                <p>Roronoa Zoro</p>
+                <img src="../imagens/layout/zoroSola.jpg" alt="Zoro">
+            </div>
+            <div class="tripulacao">
+                <p>Nami</p>
+                <img src="../imagens/layout/nami.webp" alt="Nami">
+            </div>
+            <div class="tripulacao">
+                <p>Usopp</p>
+                <img src="../imagens/layout/usopp.webp" alt="Usopp">
+            </div>
+            <div class="tripulacao">
+                <p>Sanji Vinsmoker</p>
+                <img src="../imagens/layout/sanji.jpg" alt="Sanji">
+            </div>
+            <div class="tripulacao">
+                <p>Tony Tony Chopper</p>
+                <img src="../imagens/layout/chopper.webp" alt="Chopper">
+            </div>
+            <div class="tripulacao">
+                <p>Nico Robin</p>
+                <img src="../imagens/layout/robin.webp" alt="Robin">
+            </div>
+            <div class="tripulacao">
+                <p>Franky</p>
+                <img src="../imagens/layout/Franky.webp" alt="Franky">
+            </div>
+            <div class="tripulacao">
+                <p>Brook</p>
+                <img src="../imagens/layout/brook.webp" alt="Brook">
+            </div>
+            <div class="tripulacao">
+                <p>Jinbe</p>
+                <img src="../imagens/layout/jinbe.jpg" alt="Jinbe">
+            </div>       
+        </div>
+        
+    </div>
+</body>
+</html>
